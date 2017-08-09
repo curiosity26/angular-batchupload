db4aa8851e7c1ed537c8f301f9b55802c5f38b46
curiosity26 <curiosity26@gmail.com>
2017-08-09 16:57:10 -0400

    Update Dropzone to wireup events in the directive

--- ng-batchupload.js
+++ ng-batchupload.js
@@ -31,66 +31,154 @@ angular.module('batchUpload', [])
                 };
             }];
         })
-        .directive('buDropZone', ["batchDropZone", function(batchDropZone) {
+        .directive('buDropZone', [function() {
             return {
                 restrict: 'AEC',
+                scope: {
+                    buManager: '=?',
+                    buOnError: '&?',
+                    buOnQueue: '&?',
+                    buOnComplete: '&?',
+                    buOnInvalid: '&?',
+                    buOnStart: '&?',
+                    buOnPause: '&?',
+                    buOnProgress: '&?',
+                    buOnFileStart: '&?',
+                    buUrl: '@?',
+                    buMethod: '@?',
+                    buDragOverClass: '@?',
+                    buAllowedTypes: '@?',
+                    buAllowedExtentions: '@?',
+                    buMaxFileSize: '@?',
+                    buMaxChunkSize: '@?',
+                    buMaxQueue: '@?',
+                    buFormFileField: '@?',
+                    buChunkParameter: '@?',
+                    buChunksParameter: '@?'
+                },
+                bindToController: true,
+                controller: ['$element', function($element) {
+                    var settings = {};
+
+                    this.$onInit = function() {
+
+                        if (this.buUrl) {
+                            settings.url = this.buUrl
+                        }
+
+                        if (this.buMethod) {
+                            settings.method = this.buMethod;
+                        }
+
+                        if (this.buDragOverClass) {
+                            settings.dragOverClass = this.buDragOverClass;
+                        }
+
+                        if (this.buAllowedTypes) {
+                            settings.allowedTypes = this.buAllowedTypes.split(' ');
+                        }
+
+                        if (this.buAllowedExtentions) {
+                            settings.allowedExtensions = this.buAllowedExtensions.split(' ');
+                        }
+
+                        if (this.buMaxFileSize) {
+                            settings.maxFileSize = this.buMaxFileSize;
+                        }
+
+                        if (this.buMaxChunkSize) {
+                            settings.maxChunkSize = this.buMaxChunkSize;
+                        }
+
+                        if (this.buMaxQueue) {
+                            settings.maxQueue = this.buMaxQueue;
+                        }
+
+                        if (this.buFormFileField) {
+                            settings.formFileField = this.buFormFileField;
+                        }
+
+                        if (this.buChunkParameter) {
+                            settings.chunkParameter = this.buChunkParameter;
+                        }
+
+                        if (this.buChunksParameter) {
+                            settings.chunksParameter = this.buChunksParameter;
+                        }
+
+                        settings.manager = this.buManager || null;
+
+                        this.dropzone = new FileDropZone($element[0], settings);
+
+                        if (!!this.buOnComplete) {
+                            this.dropzone.on('complete', this.buOnComplete);
+                        }
+
+                        if (!!this.buOnError) {
+                            this.dropzone.on('error', this.buOnError);
+                        }
+
+                        if (!!this.buOnFileStart) {
+                            this.dropzone.on('file_start', this.buOnFileStart);
+                        }
+
+                        if (!!this.buOnStart) {
+                            this.dropzone.on('start', this.buOnStart);
+                        }
+
+                        if (!!this.buOnPause) {
+                            this.dropzone.on('pause', this.buOnPause);
+                        }
+
+                        if (!!this.buOnProgress) {
+                            this.dropzone.on('progress', this.buOnProgress);
+                        }
+
+                        if (!!this.buOnInvalid) {
+                            this.dropzone.on('invalud', this.buOnInvalid);
+                        }
+
+                        if (!!this.buOnQueue) {
+                            this.dropzone.on('queue', this.buOnQueue);
+                        }
+                    };
+
+                    this.$onDestroy = function() {
+                        if (!!this.buOnComplete) {
+                            this.dropzone.off('complete', this.buOnComplete);
+                        }
+
+                        if (!!this.buOnError) {
+                            this.dropzone.off('error', this.buOnError);
+                        }
+
+                        if (!!this.buOnFileStart) {
+                            this.dropzone.off('file_start', this.buOnFileStart);
+                        }
+
+                        if (!!this.buOnStart) {
+                            this.dropzone.off('start', this.buOnStart);
+                        }
+
+                        if (!!this.buOnPause) {
+                            this.dropzone.off('pause', this.buOnPause);
+                        }
+
+                        if (!!this.buOnProgress) {
+                            this.dropzone.off('progress', this.buOnProgress);
+                        }
+
+                        if (!!this.buOnInvalid) {
+                            this.dropzone.off('invalud', this.buOnInvalid);
+                        }
+
+                        if (!!this.buOnQueue) {
+                            this.dropzone.off('queue', this.buOnQueue);
+                        }
+                    };
+                }],
                 link: function(scope, element, attr) {
-                    element.dropzone = batchDropZone(element);
                     element.addClass('file-drop-zone');
-
-                    if (attr['buUrl']) {
-                        element.dropzone.settings.manager.settings.url =
-                                element.dropzone.settings.url = attr['buUrl'];
-                    }
-
-                    if (attr['buMethod']) {
-                        element.dropzone.settings.manager.settings.method =
-                                element.dropzone.settings.method = attr['buMethod'];
-                    }
-
-                    if (attr['buDragOverClass']) {
-                        element.dropzone.settings.dragOverClass = attr['buDragOverClass'];
-                    }
-
-                    if (attr['buAllowedTypes']) {
-                        element.dropzone.settings.manager.settings.allowedTypes =
-                                element.dropzone.settings.allowedTypes = attr['buAllowedTypes'].split(' ');
-                    }
-
-                    if (attr['buAllowedExtentions']) {
-                        element.dropzone.settings.manager.settings.allowedExtensions =
-                                element.dropzone.settings.allowedExtensions = attr['buAllowedExtensions'].split(' ');
-                    }
-
-                    if (attr['buMaxFileSize']) {
-                        element.dropzone.settings.manager.settings.maxFileSize =
-                            element.dropzone.settings.maxFileSize = attr['buMaxFileSize'];
-                    }
-
-                    if (attr['buMaxChunkSize']) {
-                        element.dropzone.settings.manager.settings.maxChunkSize =
-                                element.dropzone.settings.maxChunkSize = attr['buMaxChunkSize'];
-                    }
-
-                    if (attr['buMaxQueue']) {
-                        element.dropzone.settings.manager.maxQueue =
-                                element.dropzone.settings.maxQueue = attr['buMaxQueue'];
-                    }
-
-                    if (attr['buFormFileField']) {
-                        element.dropzone.settings.manager.settings.formFileField =
-                                element.dropzone.settings.formFileField = attr['buFormFileField'];
-                    }
-
-                    if (attr['buChunkParameter']) {
-                        element.dropzone.settings.manager.settings.chunkParameter =
-                                element.dropzone.settings.chunkParameter = attr['buChunkParameter'];
-                    }
-
-                    if (attr['buChunksParameter']) {
-                        element.dropzone.settings.manager.settings.chunksParameter =
-                                element.dropzone.settings.chunksParameter = attr['buChunksParameter'];
-                    }
                 }
             }
         }])
